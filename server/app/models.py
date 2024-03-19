a347645d2191f380b62b0714708975ab9ff36fd3
James Richmond <jblackenmusic@gmail.com>
2024-03-18 21:33:52 -0600

    installed react and tailwind

--- server/app/models.py
+++ server/app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class TestModel(models.Model):
+    string = models.CharField(max_length=100)
+    number = models.IntegerField()
+    check = models.BooleanField()
+
+    def save(self, *args, **kwargs):
+        if self.number < 10:
+            return
+        super().save(*args, **kwargs)
--- server/app/views.py
+++ server/app/views.py
@@ -1,6 +1,25 @@
 from django.http import HttpResponse
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import TestModel
+from .serializers import TestModelSerializer
 # from django.shortcuts import render
 
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+#API views
+class TestModelEndpoints(APIView):
+    def get(self, request):
+        querySet = TestModel.objects.all()
+        serializer = TestModelSerializer(querySet, many=True)
+        return Response(serializer.data)
+    
+    def post(self, request):
+        serializer = TestModelSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
