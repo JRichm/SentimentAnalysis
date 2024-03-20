5000fa42ebb361d8ae00fa58cceae4ffeab570fd
James Richmond <jblackenmusic@gmail.com>
2024-03-19 22:15:34 -0600

    api fetches and frontend displays data correctly

--- server/api/urls.py
+++ server/api/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('testformendpoint/', views.EndpointView),
+    path('get_csrf_token/', views.get_csrf_token),
 ]
--- server/api/views.py
+++ server/api/views.py
@@ -1,15 +1,22 @@
 import json
 from django.http import JsonResponse
+from django.middleware.csrf import get_token
 
 def EndpointView(request):
-    if request.method == 'GET':
-        # Handle GET request
-        data = {'message': 'This is a GET request', 'data': request.GET.dict()}
-        return JsonResponse(data)
-    elif request.method == 'POST':
-        # Handle POST request
-        data = {'message': 'This is a POST request', 'data': request.POST.dict()}
-        return JsonResponse(data)
+    if request.method == 'POST':
+        try:
+            # Parse the JSON data from the request body
+            json_data = json.loads(request.body)
+            data = {'message': 'request received', 'data': json_data}
+            print(data)
+            return JsonResponse(data)
+        except json.JSONDecodeError:
+            return JsonResponse({'error': 'Invalid JSON data'}, status=400)
     else:
-        # Handle other request methods
-        return JsonResponse({'error': 'Method not allowed'}, status=405)
+        # Handle other request methods if needed
+        return JsonResponse({'error': 'Only POST requests are allowed'}, status=405)
+        
+
+def get_csrf_token(request):
+    csrf_token = get_token(request)
+    return JsonResponse({'csrfToken': csrf_token})
