949c8f85e4fda3742105b07326f1650014bc9102
James Richmond <jblackenmusic@gmail.com>
2024-03-21 16:49:59 -0600

    updated server url paths

--- server/server/urls.py
+++ server/server/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import TemplateView  # Add this import
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='index.html')),
     path('api/', include('api.urls')),
     path('app/', include('app.urls')),
     path('admin/', admin.site.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
